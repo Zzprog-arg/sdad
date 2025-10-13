24885b77767df20e525f0ae679343d7c9413be39
Zzprog-arg <mateopaz201@gmail.com>
2025-10-13 00:37:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,14 @@ import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 import fsSync from "fs";
-const app = express();
-// ... otras importaciones
-
-// justo después de crear `app = express();`
-app.use(express.static(path.join(__dirname, "public")));
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "public")));
 // CONFIG
 const CONFIG = {
   xtreamUser: process.env.XTREAM_USER || "demo",
